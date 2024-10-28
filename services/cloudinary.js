14aed201d9e186a67c4bcb9eb9dd0c743d415c96
shaikhasif69 <theshaikhasif03@gmail.com>
2024-10-28 22:34:11 +0530

    .gitignore is now working

---
+++ services/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: "dtgdwiyxm",
+  api_key: "769694859461743",
+  api_secret: "dJxxcda-sDrkqDtcthVXoPJ7AR0",
+});
+
+export default cloudinary;
